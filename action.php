7b4f5f5ee55ce47885799e1b89b6ba6db4211cf9
Lukas <13336216+LukBowen@users.noreply.github.com>
2021-01-08 14:30:17 -0800

    Update action.php

--- action.php
+++ action.php
@@ -3,7 +3,7 @@
  * DokuWiki Plugin drestrict (Action Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
- * @author  Danny Götte <danny.goette@fem.tu-ilmenau.de>
+ * @author  Danny GÃ¶tte <danny.goette@fem.tu-ilmenau.de>
  */
 
 // must be run within Dokuwiki
@@ -20,16 +20,17 @@ class action_plugin_drestrict extends DokuWiki_Action_Plugin {
     // stop if domains do not match, thanks to dokuwiki the eMail has the magic number 3, which may change in future versions
 	const EMAIL_INDEX = 3;
 
-    public function register(Doku_Event_Handler &$controller) {
+    public function register(Doku_Event_Handler $controller) {
 
        $controller->register_hook('AUTH_USER_CHANGE', 'BEFORE', $this, 'handle_auth_user_change');
    
     }
 
-    public function handle_auth_user_change(Doku_Event &$event, $param) {
+    public function handle_auth_user_change(Doku_Event $event, $param) {
 
         // check for domain setting        
-        $confDomain = $this->getConf('domain');
+        $confDomain = $this->getConf('email_domains');
+        $confErrorMessage = $this->getConf('error_message');
         if (trim($confDomain) == false) {
             return true;
         }
@@ -45,8 +46,8 @@ class action_plugin_drestrict extends DokuWiki_Action_Plugin {
 
         $email = explode('@', $event->data['params'][self::EMAIL_INDEX]);
         $domain = $email[1];
-        if (strtolower($domain) != strtolower($confDomain)) {
-            msg(sprintf($this->getLang('wrong_domain'), $confDomain), -1);
+        if (!in_array(strtolower($domain), $confDomain) {
+            msg(sprintf($this->getLang('wrong_domain'), $confErrorMessage), -1);
 //            $event->stopPropagation();
             $event->preventDefault();
             $event->data['params'] = array();
